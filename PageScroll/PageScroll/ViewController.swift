9ac0a457160663a722a188b0cac8994b794bb853
ABnyc <arpita.nyc@gmail.com>
2017-01-23 17:38:04 -0500

    1. Scrolling with non-full screen size scroll view. 2. Code added to make scroll work when swiped outside scroll view

--- PageScroll/PageScroll/ViewController.swift
+++ PageScroll/PageScroll/ViewController.swift
@@ -19,21 +19,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        var contentWidth: CGFloat = 0.0
         
+        //***dragg the Pan Gesture Recognizer into the View Controller and write the following codeline for scroll to work when swiped outside scroll view
+        
+        view.addGestureRecognizer(scrollView.panGestureRecognizer)
+    }
+    //moved code from view did load to view did appear when scroll view was not same as full screen, as scroll view frame sizes are available in view did appear and not view did load
+    
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        let scrollWidth = scrollView.frame.size.width
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")
             let imageView = UIImageView(image: image)
             images.append(imageView)
             
             var newX: CGFloat = 0.0
-            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            newX = scrollWidth / 2 + scrollWidth  * CGFloat(x)
             contentWidth += newX
-
+            
             scrollView.addSubview(imageView)
-            imageView.frame = CGRect(x: newX - 75, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
         }
-        
+        //scrollView.backgroundColor = UIColor.blue
+        scrollView.clipsToBounds = false
         scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
     }
 
