c7dd5d06c532cdcac9312d4af7bcbfc4700d2eec
ABnyc <arpita.nyc@gmail.com>
2017-01-25 12:30:39 -0500

    Starting retro calculator project

---
+++ RetroCalculator/RetroCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RetroCalculator
+//
+//  Created by Arpita Bhatia on 1/25/17.
+//  Copyright © 2017 Arpita Bhatia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
