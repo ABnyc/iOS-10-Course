0d51365725048140232e5260a84b1918d90b54b4
ABnyc <arpita.nyc@gmail.com>
2017-01-25 17:25:43 -0500

    Sound implememnted for button 0 to 9

--- RetroCalculator/RetroCalculator/ViewController.swift
+++ RetroCalculator/RetroCalculator/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var btnSoundPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        do {
+            try btnSoundPlayer = AVAudioPlayer(contentsOf: soundURL)
+            btnSoundPlayer.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+    }
+    
+    @IBAction func numberBtnPressed(sender: UIButton) {
+        
+        playSound()
+        
     }
 
+    func playSound() {
+        if btnSoundPlayer.isPlaying {
+            btnSoundPlayer.stop()
+        }
+        btnSoundPlayer.play()
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
