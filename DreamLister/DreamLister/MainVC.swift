9025f136e33235e2ee7c3cbdf4f2226ef93563cd
ABnyc <arpita.nyc@gmail.com>
2017-01-31 13:00:45 -0500

    UI created, IBoutlets connected,

---
+++ DreamLister/DreamLister/MainVC.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  DreamLister
+//
+//  Created by Arpita Bhatia on 1/30/17.
+//  Copyright © 2017 Arpita Bhatia. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var segment: UISegmentedControl!
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    
+    
+    
+    
+
+}
+
---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Arpita Bhatia on 1/31/17.
+//  Copyright © 2017 Arpita Bhatia. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+   
+
+}
