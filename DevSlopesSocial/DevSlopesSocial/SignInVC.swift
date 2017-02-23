f3c85784341b5fdb55c97ee13ef15bdf4cc855e2
ABnyc <arpita.nyc@gmail.com>
2017-02-23 18:22:42 -0500

    Completed setting up Firebas integration

---
+++ DevSlopesSocial/DevSlopesSocial/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevSlopesSocial
+//
+//  Created by Arpita Bhatia on 2/23/17.
+//  Copyright © 2017 Arpita Bhatia. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
